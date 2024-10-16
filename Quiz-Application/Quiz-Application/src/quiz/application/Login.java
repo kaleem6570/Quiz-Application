d1d7c621ce1f0a71e3796a0fdc2e9da8980a9c45
kaleem6570 <imkaleem135@gmail.com>
2024-10-16 10:49:58 +0530

    edited a file

---
+++ Quiz-Application/Quiz-Application/src/quiz/application/Login.java
@@ -0,0 +1,34 @@
+
+package quiz.application;
+import java.awt.*;
+import javax.swing.*;
+public class Login extends JFrame{
+    JLabel name;
+    JTextField name1;
+    Login(){
+        setLayout(null);
+        setTitle("Quiz");
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/1.png"));
+        Image i2 = i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
+        ImageIcon i3 =new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0,10,550,500);
+        add(image);
+        
+         name  = new JLabel("Name");
+         name.setBounds(100,140,100,35);
+         name.setFont(new Font("System", Font.BOLD,20));
+         add(name);
+        
+         getContentPane().setBackground(Color.WHITE);
+        
+         setSize(1000,490);
+         setLocation(300,200);
+         setVisible(true);
+       
+    }
+    public static void main(String args[]){
+        new Login();
+    }
+}
